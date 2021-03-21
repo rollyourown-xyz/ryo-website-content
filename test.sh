497acbc8475e40fecf598256712c6cc0885906cb
xyzroller <xyzroller@rollyourown.xyz>
2021-03-21 15:23:40 +0100

    Added script for testing local changes

---
+++ test.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# Hugo binary should be present in the folder directly above this one 
+# Run hugo with theme in ../hugo-theme
+../hugo server --themesDir ../ --theme hugo-theme --verbose
