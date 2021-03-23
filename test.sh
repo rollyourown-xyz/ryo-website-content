9c7a7379a86309045b187b235d23e74eb8bdd4b2
xyzroller <xyzroller@rollyourown.xyz>
2021-03-23 19:03:55 +0100

    Set --noHTTPCache in test.sh

--- test.sh
+++ test.sh
@@ -2,4 +2,4 @@
 
 # Hugo binary should be present in the folder directly above this one 
 # Run hugo with theme in ../hugo-theme
-../hugo server --themesDir ../ --theme hugo-theme --verbose
+../hugo server --themesDir ../ --theme hugo-theme --verbose --noHTTPCache
