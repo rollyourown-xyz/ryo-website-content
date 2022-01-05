223002ef9dd3e82568512b2105c4b58f9b3be443
xyzroller <xyzroller@rollyourown.xyz>
2022-01-05 16:26:46 +0100

    Add note on exporting site with wget

--- test_drafts.sh
+++ test_drafts.sh
@@ -3,3 +3,8 @@
 # Hugo binary should be present in the folder directly above this one 
 # Run hugo with theme in ../hugo-theme
 ../hugo server --themesDir ../ --theme hugo-theme --verbose --noHTTPCache --buildDrafts
+
+
+# To export the site to the filesystem, run this script, cd to the directory where the site
+# should be exported and then run the following command:
+# wget --recursive --convert-links --html-extension --restrict-file-names=windows --page-requisites --no-parent --domains localhost localhost:1313
