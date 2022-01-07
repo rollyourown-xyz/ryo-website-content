0de95e12fa63ba629fcefc65d4bf16befd386341
xyzroller <xyzroller@rollyourown.xyz>
2022-01-07 18:58:13 +0100

    Move scripts to hugo-scaffold repository

--- test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# Hugo binary should be present in the folder directly above this one 
-# Run hugo with theme in ../hugo-theme
-../hugo server --themesDir ../ --theme hugo-theme --verbose --noHTTPCache
